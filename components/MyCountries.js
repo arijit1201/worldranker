586b807b7aa4a1adaf18c6f0343a29b3076c2f99
noir <noir@LILITH.localdomain>
2022-08-07 02:23:05 +0530

    fixed key

--- components/MyCountries.js
+++ components/MyCountries.js
@@ -14,19 +14,19 @@ const MyCountries = ({ countries }) => {
         <div className={styles.namecol}>
           <h1>Name</h1>
           {visibleCountries.map((country) => (
-            <p className={styles.namebold}>{country.name.common}</p>
+            <p key={country.name.common} className={styles.namebold}>{country.name.common}</p>
           ))}
         </div>
         <div className={styles.namecol}>
           <h1>Area</h1>
           {visibleCountries.map((country) => {
-            return <p>{country.area}</p>;
+            return <p key={country.name.common}>{country.area}</p>;
           })}
         </div>
         <div className={styles.namecol}>
           <h1>Population</h1>
           {visibleCountries.map((country) => (
-            <p>{country.population}</p>
+            <p key={country.name.common}>{country.population}</p>
           ))}
         </div>
       </div>
